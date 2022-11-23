f21e913772713d700030f76361a27d48bc15941c
Konstantinos Mavrogeorgis <blackgeorge1997@gmail.com>
2022-11-23 03:46:31 +0200

    Testing code added

---
+++ code/example1.py
@@ -0,0 +1,15 @@
+import pyspark as sp
+import random
+sc = sp.SparkContext(appName='myApp')
+
+NUM_SAMPLES = 1000
+
+def inside(p):
+    x, y = random.random(), random.random()
+    return x*x + y*y < 1
+
+count = sc.parallelize(range(0, NUM_SAMPLES)) \
+             .filter(inside).count()
+print("Pi is roughly %f" % (4.0 * count / NUM_SAMPLES))
+
+sc.close()
